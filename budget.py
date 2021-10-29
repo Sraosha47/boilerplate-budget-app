5b6c1aa55a35eb36806b90b395617760cda92376
squoshy <70937539+Sraosha47@users.noreply.github.com>
2021-10-29 11:50:31 +0000

    Added __str__ method to the class.

--- budget.py
+++ budget.py
@@ -2,7 +2,26 @@ class Category:
     def __init__(self, category):
         self.category = category
         self.ledger = []
-        
+        self.printout = ""
+        self.stars = int(((30 - len(category))/ 2))* "*"
+        self.title = self.stars + self.category + self.stars + "\n"
+        self.printout += self.title
+
+    def __str__(self):
+        for item in self.ledger:
+            description = self.ledger[self.ledger.index(item)]["description"]
+            print("description: " + description + "\n")
+            amount = self.ledger[self.ledger.index(item)]["amount"]
+            print("amount: "+ str(amount) + "\n")
+            item_line = ""
+            if len(description) > 23:
+                item_line = description[0:23] +  (30 - 23 - len(str(amount))) *" " +str(amount) + "\n"
+                self.printout += item_line
+            else:
+                item_line = description +  (30 - len(description) - len(str(amount))) *" " +str(amount) + "\n"
+                self.printout += item_line            
+        return(self.printout)
+
     def deposit(self, amount, description=''):
         self.ledger.append({"amount": amount, "description": description})
         
